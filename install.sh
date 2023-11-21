5a3a45a01a5c52fe23f4f657628560e6d3e07b58
marcofariasmx <marcofariasmx@gmail.com>
2023-11-20 19:38:01 -0600

    FIX: Fixed auto boot of program

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ SCRIPT_DIR="$(dirname "$0")"
 # Create the tmux starter script
 cat <<EOL > $SCRIPT_DIR/start_in_tmux.sh
 #!/bin/bash
-tmux new-session -d -s cam_session "sudo python3 $SCRIPT_DIR/flask_picam2_stream_and_pic.py"
+tmux new-session -d -s cam_session "sudo python3 $SCRIPT_DIR/Raspberry-Picamera2-Surveillance/flask_picam2_stream_and_pic.py"
 EOL
 
 # Make the script executable
@@ -31,7 +31,7 @@ chmod +x $SCRIPT_DIR/start_in_tmux.sh
 sudo cp /etc/rc.local /etc/rc.local.backup
 # Use awk to insert the commands before 'exit 0'
 # This disables wifi power management which causes interruptions and automatically runs the camera script at boot up
-awk -v user="$CURRENT_USER" -v dir="$SCRIPT_DIR" '/^exit 0/ { print "/sbin/iwconfig wlan0 power off"; print "sudo -u " user " bash " dir "/start_in_tmux.sh &"; } { print; }' /etc/rc.local.backup | sudo tee /etc/rc.local
+awk -v user="$CURRENT_USER" -v dir="$SCRIPT_DIR" '/^exit 0/ { print "/sbin/iwconfig wlan0 power off"; print "sudo -u " user " bash " dir "/Raspberry-Picamera2-Surveillance/start_in_tmux.sh &"; } { print; }' /etc/rc.local.backup | sudo tee /etc/rc.local
 
 # Change swappiness permanently
 echo "vm.swappiness=5" | sudo tee -a /etc/sysctl.conf
